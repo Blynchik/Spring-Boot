26bdedefec58d09cbc3beaa2640d8ac45415e344
Vadim Sovetnikov <vadimsovetnikov@mail.ru>
2023-04-26 13:53:28 +0300

    8.4

--- src/main/java/com/zaurtregulov/spring/springboot/spring_course/springboot/controller/MyRestController.java
+++ src/main/java/com/zaurtregulov/spring/springboot/spring_course/springboot/controller/MyRestController.java
@@ -20,44 +20,44 @@ public class MyRestController {
         return employeeService.getAllEmployees();
     }
 
-//    @GetMapping("/employees/{id}")
-//    public Employee getEmployee(@PathVariable int id) {
-//        Employee employee = employeeService.getEmployee(id);
-//
+    @GetMapping("/employees/{id}")
+    public Employee getEmployee(@PathVariable int id) {
+        Employee employee = employeeService.getEmployee(id);
+
 //        if (employee == null) {
 //            throw new NotSuchEmployeeException("No employee id " + id);
 //        }
-//
-//        return employee;
-//    }
-//
-//    @PostMapping("/employees")
-//    public Employee addNewEmployee(@RequestBody Employee employee) {
-//
-//        employeeService.saveEmployee(employee);
-//
-//        return employee;
-//    }
-//
-//    @PutMapping("/employees")
-//    public Employee updateEmployee(@RequestBody Employee employee) {
-//
-//        employeeService.saveEmployee(employee);
-//
-//        return employee;
-//    }
-//
-//    @DeleteMapping("/employees/{id}")
-//    public String deleteEmployee(@PathVariable int id) {
-//
-//        Employee employee = employeeService.getEmployee(id);
-//
+
+        return employee;
+    }
+
+    @PostMapping("/employees")
+    public Employee addNewEmployee(@RequestBody Employee employee) {
+
+        employeeService.saveEmployee(employee);
+
+        return employee;
+    }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee) {
+
+        employeeService.saveEmployee(employee);
+
+        return employee;
+    }
+
+    @DeleteMapping("/employees/{id}")
+    public String deleteEmployee(@PathVariable int id) {
+
+        Employee employee = employeeService.getEmployee(id);
+
 //        if (employee == null) {
 //            throw new NotSuchEmployeeException("No employee " + id);
 //        }
-//
-//        employeeService.deleteEmployee(id);
-//
-//        return "Employee " + id + " deleted";
-//    }
+
+        employeeService.deleteEmployee(id);
+
+        return "Employee " + id + " deleted";
+    }
 }

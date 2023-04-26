42452100dc8de793e476ee84b958444c36bf9de1
Vadim Sovetnikov <vadimsovetnikov@mail.ru>
2023-04-26 13:16:48 +0300

    spring boot 8.2

---
+++ src/test/java/com/zaurtregulov/spring/springboot/spring_course/springboot/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zaurtregulov.spring.springboot.spring_course.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

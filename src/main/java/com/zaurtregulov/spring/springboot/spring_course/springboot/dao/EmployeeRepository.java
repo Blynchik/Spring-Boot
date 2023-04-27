5ee4616ed7093980e83c97b5c3882df9e9b6f916
Vadim Sovetnikov <vadimsovetnikov@mail.ru>
2023-04-27 13:07:41 +0300

    8.7

--- src/main/java/com/zaurtregulov/spring/springboot/spring_course/springboot/dao/EmployeeRepository.java
+++ src/main/java/com/zaurtregulov/spring/springboot/spring_course/springboot/dao/EmployeeRepository.java
@@ -4,10 +4,9 @@ package com.zaurtregulov.spring.springboot.spring_course.springboot.dao;
 
 import com.zaurtregulov.spring.springboot.spring_course.springboot.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
-    public List<Employee> findAllByName(String name);
 }
